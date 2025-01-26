99754f40378da5c6e233e014b093fcc997d5337d
Trey Gardner <treygardner72@gmail.com>
2025-01-26 15:12:20 -0700

    creating file and path

--- src/AssignmentsSourceCode/GardnerTreyAssignment1.java
+++ src/AssignmentsSourceCode/GardnerTreyAssignment1.java
@@ -7,15 +7,21 @@
  * reading/writing from files
 package AssignmentsSourceCode;
 */
+//package for all assignments
 package AssignmentsSourceCode;
 
+//Importing Java packages
 import java.util.Arrays;
+import java.io.File;
 
+//assignment class
 public class GardnerTreyAssignment1 {
+    
     public static void main(String[] args){
         int[] numbers = {1, 18, 10, 2, 16, 8, 15, 9, 9, 17, 14, 18, 1, 19, 18, 2, 1};
         System.out.println(numbers);
         Arrays.sort(numbers);         
-        File NumbersFile = new File("assignment1.txt");
-    }
+        File numbersFile = new File("assignment1.txt");
+        
+}
 }
