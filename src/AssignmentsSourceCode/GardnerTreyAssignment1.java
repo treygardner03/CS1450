b09c8e16ccc9860a389e3c1ee9228bf77991c2da
Trey Gardner <treygardner72@gmail.com>
2025-01-26 12:50:14 -0700

    Creating Assignment 1

---
+++ src/AssignmentsSourceCode/GardnerTreyAssignment1.java
@@ -0,0 +1,21 @@
+
+/*Name: Trey Gardner
+ * Class: CS1450(M/W)
+ * Due: Jan 29, 2025
+ * Assignment #1
+ * DESC: This program will show proficiency in creating an manipulating arrays and
+ * reading/writing from files
+package AssignmentsSourceCode;
+*/
+package AssignmentsSourceCode;
+
+import java.util.Arrays;
+
+public class GardnerTreyAssignment1 {
+    public static void main(String[] args){
+        int[] numbers = {1, 18, 10, 2, 16, 8, 15, 9, 9, 17, 14, 18, 1, 19, 18, 2, 1};
+        System.out.println(numbers);
+        Arrays.sort(numbers);         
+        File NumbersFile = new File("assignment1.txt");
+    }
+}
