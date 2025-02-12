966f316f5e88bfba6e1a4d5695364fc83b32d8f8
Trey Gardner <treygardner72@gmail.com>
2025-02-12 14:53:15 -0700

    Update GardnerTreyAssignment3.java

--- src/AssignmentsSourceCode/GardnerTreyAssignment3.java
+++ src/AssignmentsSourceCode/GardnerTreyAssignment3.java
@@ -30,7 +30,6 @@ public class GardnerTreyAssignment3 {
 	//Task C: After animals are in array, Perform 3 tasks
 		String read_name;
 		String read_species;
-		String read_noise; 
 		int read_skill_swimming;
 		int read_skill_running;
 		int read_skill_climbing;
@@ -44,23 +43,23 @@ public class GardnerTreyAssignment3 {
 			read_skill_climbing = reader.nextInt();
 			
 			if(read_species.equals("Alligator")) {
-				all_animals[i] = new Alligator(read_name, read_noise, read_skill_swimming, read_skill_running, read_skill_climbing);
+				all_animals[i] = new Alligator(read_name, read_skill_swimming, read_skill_running, read_skill_climbing);
 			}
 
 			if(read_species.equals("Bear")) {
-				all_animals[i] = new Bear(read_name, read_noise, read_skill_swimming, read_skill_running, read_skill_climbing);
+				all_animals[i] = new Bear(read_name, read_skill_swimming, read_skill_running, read_skill_climbing);
 			}
 
 			if(read_species.equals("Giraffe")) {
-				all_animals[i] = new Giraffe(read_name, read_noise, read_skill_swimming, read_skill_running, read_skill_climbing);
+				all_animals[i] = new Giraffe(read_name, read_skill_swimming, read_skill_running, read_skill_climbing);
 			}
 
 			if(read_species.equals("Monkey")) {
-				all_animals[i] = new Monkey(read_name, read_noise, read_skill_swimming, read_skill_running, read_skill_climbing);
+				all_animals[i] = new Monkey(read_name, read_skill_swimming, read_skill_running, read_skill_climbing);
 			}
 
 			if(read_species.equals("Sloth")) {
-				all_animals[i] = new Sloth(read_name, read_noise, read_skill_swimming, read_skill_running, read_skill_climbing);
+				all_animals[i] = new Sloth(read_name, read_skill_swimming, read_skill_running, read_skill_climbing);
 			}
 		}
 		
@@ -82,6 +81,174 @@ public class GardnerTreyAssignment3 {
 
 }
 
-class  Animal {
+interface Swimmer {
+	public abstract int swim();
+}
+
+interface Runner {
+	public abstract int run();
+}
+
+interface Climber {
+	public abstract int climb();
+}
+
+abstract class Animal {
 	
+	private String name;
+	private String species;
+	
+	public void set_name(String name) {
+		this.name = name;
+	}
+	
+	public void set_species(String species) {
+		this.species = species;
+	}
+	
+	public String get_name() {
+		return this.name;
+	}
+	
+	public String get_species() {
+		return this.species;
+	}
+	
+	public abstract String make_noise();
+
+}
+
+class Alligator extends Animal implements Swimmer, Runner{
+
+	private int swim_speed;
+	private int run_speed;
+	
+	public Alligator(String name, int swim_speed, int run_speed) {
+		set_name(name);
+		set_species("Alligator");
+		this.swim_speed = swim_speed;
+		this.run_speed = run_speed;
+	}
+	
+	@Override
+	public String make_noise() {
+		return "Crunch";
+	}
+	
+	@Override
+	public int swim() {
+		return this.swim_speed;
+	}
+	
+	@Override
+	public int run() {
+		return this.run_speed;
+	}
+}
+
+class Bear extends Animal implements Swimmer, Runner, Climber {
+
+	private int swim_speed;
+	private int run_speed;
+	private int climb_speed;
+	
+	public Bear(String name, int swim_speed, int run_speed, int climb_speed) {
+		set_name(name);
+		set_species("Bear");
+		this.swim_speed = swim_speed;
+		this.run_speed = run_speed;
+		this.climb_speed = climb_speed;
+	}
+
+	public String make_noise() {
+		return "Growl";
+	}
+	
+	@Override
+	public int swim() {
+		return this.swim_speed;
+	}
+	
+	@Override
+	public int run() {
+		return this.run_speed;
+}
+	
+	@Override
+	public int climb() {
+		return this.climb_speed;
+	}
+}
+
+class Giraffe extends Animal implements Runner {
+	
+	private int run_speed;
+	
+	public Giraffe(String name, int run_speed) {
+		set_name(name);
+		set_species("Giraffe");
+		this.run_speed = run_speed;
+	}
+
+	public String make_noise() {
+		return "Bleat";
+	}
+	
+	@Override
+	public int run() {
+		return this.run_speed;
+	}
+}
+	
+class Monkey extends Animal implements Runner, Climber {
+	
+	private int run_speed;
+	private int climb_speed;
+	
+	public Monkey(String name, int run_speed, int climb_speed) {
+		set_name(name);
+		set_species("Monkey");
+		this.run_speed = run_speed;
+		this.climb_speed = climb_speed;
+	}
+	
+	public String make_noise() {
+		return "Screech";
+	}
+	
+	@Override
+	public int run() {
+		return this.run_speed;
+}
+	
+	@Override
+	public int climb() {
+		return this.climb_speed;
+	}
+}
+
+class Sloth extends Animal implements Swimmer, Climber {
+	
+	private int swim_speed;
+	private int climb_speed;
+	
+	public Sloth(String name, int swim_speed, int climb_speed) {
+		set_name(name);
+		set_species("Sloth");
+		this.swim_speed = swim_speed;
+		this.climb_speed = climb_speed;
+	}
+
+	public String make_noise() {
+		return "Squeak";
+	}
+
+	@Override
+	public int swim() {
+		return this.swim_speed;
+	}
+	@Override
+	public int climb() {
+		return this.climb_speed;
+	}
 }
