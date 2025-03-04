fdfea5718051d8a46e927d82114b478b1196f300
Trey Gardner <treygardner72@gmail.com>
2025-03-04 14:45:00 -0700

    assignment 5 setup

---
+++ src/AssignmentsSourceCode/GardnerTreyAssignment5.java
@@ -0,0 +1,26 @@
+/* Name: Trey Gardner
+   Class: CS1450 (M/W)
+   Due: March 5th, 2025
+   Assignment: #5
+   Description: This program will; create 2 JFC stacks of integers read from files,
+   sort the stacks, and the merge the two stacks into one sorted stack.
+ */
+package AssignmentsSourceCode;
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.Stack;
+
+public class GardnerTreyAssignment5 {
+    public static void main(String[] args) {
+        int[] numbers = {0, 0, 4, 3, 0, 2, 1, 0, 0};
+        Stack<Integer> numbers_stack = new Stack<Integer>();
+        replaceZerosWithTen(numbers_stack);
+
+    }
+    public static void replaceZerosWithTen(Stack<Integer> numbers_stack) {
+        for (int i = 0; i < numbers_stack.size(); i++) {
+
+        }
+    }
+}
