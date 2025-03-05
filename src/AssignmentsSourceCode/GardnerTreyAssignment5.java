9da436ce18d09298c05e2b7b33b52554b0c7d0ce
Trey Gardner <treygardner72@gmail.com>
2025-03-05 11:37:09 -0700

    GENERIC - stacks and methods

--- src/AssignmentsSourceCode/GardnerTreyAssignment5.java
+++ src/AssignmentsSourceCode/GardnerTreyAssignment5.java
@@ -11,6 +11,7 @@ package AssignmentsSourceCode;
 
 //Java imports
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
@@ -24,6 +25,7 @@ public class GardnerTreyAssignment5 {
         File integers_2 = new File("integers2.txt");
         File strings_1 = new File("strings1.txt");
         File strings_2 = new File("strings2.txt");
+
         //array of integers
         int[] numbers = {0, 0, 4, 3, 0, 2, 1, 0, 0};
         //empty stack
@@ -40,10 +42,17 @@ public class GardnerTreyAssignment5 {
         //creating integer stacks from generic class
         Generic_Stack<Integer> int_stack_1 = new Generic_Stack<Integer>();
         Generic_Stack<Integer> int_stack_2 = new Generic_Stack<Integer>();
+        Generic_Stack<String> string_stack_1 = new Generic_Stack<String>();
+        Generic_Stack<String> string_stack_2 = new Generic_Stack<String>();
 
         //filling integer stacks with values
-        fill_integer_stack(int_stack_1);
-        fill_integer_stack(int_stack_2);
+        fill_integer_stack(int_stack_1, integers_1);
+        fill_integer_stack(int_stack_2, integers_2);
+
+        //filling string stacks with values
+        fill_string_stack(string_stack_1, strings_1);
+        fill_string_stack(string_stack_2, strings_2);
+
     }
     //Replaces all 0's in stack with 10's
     public static void replace_zero_with_10(Stack<Integer> int_stack) {
@@ -60,6 +69,10 @@ public class GardnerTreyAssignment5 {
             int_stack.push(temp_stack.pop());
         }//for (restoring original stack)
     }//replace_zeros_with_ten
+
+    public static <E> void sort_stack(Generic_Stack<E> stack) {
+
+    }
 
     //printing stack then restoring to original
     public static void print_stack(Stack<Integer> int_stack) {
@@ -80,12 +93,41 @@ public class GardnerTreyAssignment5 {
         }//for (restore original stack)
     }//print_stack
 
-    public static void fill_integer_stack(Stack<Integer> int_stack, File integers) {
+   public static <E> void print_stack(Generic_Stack<E> stack) {
 
-    }
+   }
+
+    public static void fill_integer_stack(Generic_Stack<Integer> int_stack, File integers) {
+        try {
+            Scanner reader = new Scanner(integers);
+            while (reader.hasNextInt()) {
+                int_stack.add(reader.nextInt());
+            }//while loop
+        } catch (IOException E) {
+            System.out.println(E.getMessage());
+        }//try/catch
+    }//fill_integer_stack
+
+    public static void fill_string_stack(Generic_Stack<String> string_stack, File strings) {
+        try {
+            Scanner reader = new Scanner(strings);
+            while (reader.hasNextInt()) {
+                string_stack.add(reader.next());
+            }//while loop
+        } catch (IOException E) {
+            System.out.println(E.getMessage());
+        }//try/catch
+    }//fill_string_stack
 
 }//Assignment Class
 
 class Generic_Stack<E> {
-   ArrayList<E> list = new ArrayList<E>();
-}
+    //private data fields
+    private ArrayList<E> list = new ArrayList<E>();
+
+    //public methods
+    public void add(E e) {
+        list.add(e);
+    }//add body
+
+}//Generic Stack Class
