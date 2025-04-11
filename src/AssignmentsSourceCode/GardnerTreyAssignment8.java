fb94e85f6339336554cd241eca9453c53b1564aa
Trey Gardner <treygardner72@gmail.com>
2025-04-10 23:05:16 -0600

    finished assignment 8 moved .txt files

--- src/AssignmentsSourceCode/GardnerTreyAssignment8.java
+++ src/AssignmentsSourceCode/GardnerTreyAssignment8.java
@@ -1,4 +1,5 @@
-/*Name: Trey Gardner
+
+/* Name: Trey Gardner
 Class: CS1450 (M/W)
 Due: April 10th, 2025
 Assignment: #8
@@ -6,47 +7,104 @@ Description: This program will decrypt the secret parrot message to find their l
 will be done using 2D arrays.
  */
 
-//removing before turning in...
+//remove before turning in
 package AssignmentsSourceCode;
+
+//imports
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.lang.Character;
 
 
 //Assignment class
 public class GardnerTreyAssignment8 {
     //main
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         //Step 1: prepare to decode messages
 
-            //A. Open the 3 files for reading
+        //A. Open the 3 files for reading
+        File key_grid_text = new File("KeyGrid.txt");
+        Scanner read_key_grid = new Scanner(key_grid_text);
+        File message_1_text = new File("Message1.txt");
+        Scanner read_message_1 = new Scanner(message_1_text);
+        File message_2_text = new File("Message2.txt");
+        Scanner read_message_2 = new Scanner(message_2_text);
 
-            //B. Create a Decoding_Machine object
+        //getting key grid information
+        int num_rows = read_key_grid.nextInt();
+        int num_columns = read_key_grid.nextInt();
 
-            //C. Create and fill and ArrayList with the characters in the file "KeyGrid.txt"
+        //B. Create a Decoding_Machine object
+        Decoding_Machine decoder = new Decoding_Machine(num_rows, num_columns);
 
-            //D. Create an iterator for the ArrayList
+        //C. Create and fill an ArrayList with the characters in the file "KeyGrid.txt"
+        ArrayList<Character> key_grid_list = new ArrayList<Character>();
+        for (int i = 0; i < (num_rows * num_columns); i++) {
+            key_grid_list.add(read_key_grid.next().charAt(0));
+        }
 
-            //E. Fill the Decoding_Machine's key grid
+        //D. Create an iterator for the ArrayList
+        Iterator<Character> iterator = key_grid_list.iterator();
 
-            //F. Print the Decoding_Machine's key grid
+        //E. Fill the Decoding_Machine's key grid
+        decoder.fill_key_grid(iterator);
+
+        //F. Print the Decoding_Machine's key grid
+        decoder.print_key_grid();
 
         //Step 2: read secret message in a file
 
-            //A. Create 2 queues to store Key_Grid_Element objects
+        //A. Create 2 queues to store Key_Grid_Element objects
+        Queue<Key_Grid_Element> queue_1 = new LinkedList<>();
+        Queue<Key_Grid_Element> queue_2 = new LinkedList<>();
 
-            //B. Write a method that fills a queue with the file values
+        //B. Write a method that fills a queue with the file values
+        fill_queue(queue_1, read_message_1);
+        fill_queue(queue_2, read_message_2);
 
         //Step 3: decode the messages
 
-            //A. Create an iterator for each queue ^
+        //A. Create an iterator for each queue ^
+        Iterator<Key_Grid_Element> message_1_iterator = queue_1.iterator();
+        Iterator<Key_Grid_Element> message_2_iterator = queue_2.iterator();
 
-            //B. Call decode_message()
+        //decoding 1st secret message
+        Iterator<Character> secret_message_iterator = decoder.decode_message(message_1_iterator);
 
-            //C. Display the characters of the decoded secret message in the returned iterator
+        //printing 1st secret message:
+        System.out.println("\nSecret Message #1: \n");
+        while(secret_message_iterator.hasNext()) {
+            System.out.println(secret_message_iterator.next());
+        }
 
-            //D. Repeat b and c for 2nd message
+        //decoding 2nd secret message
+        secret_message_iterator = decoder.decode_message(message_2_iterator);
 
+        //printing 2nd secret message
+        System.out.println("\nSecret Message #2: \n");
+        while(secret_message_iterator.hasNext()) {
+            System.out.println(secret_message_iterator.next());
+        }
+
+
+    }//main
+
+    //methods
+    public static void fill_queue(Queue<Key_Grid_Element> queue, Scanner message) {
+        //declaring message length
+
+        while (message.hasNext()) {
+            queue.offer(new Key_Grid_Element(message.nextInt(), message.nextInt()));
+        }
     }
-}
+
+}//Assignment Class
 
 //Classes:
 
@@ -80,23 +138,50 @@ class Decoding_Machine {
     //2D array of characters
     private int num_rows;
     private int num_columns;
+    private char[][] key_grid;
+    private ArrayList<Character> secret_message;
 
 //public methods
     //constructor
     public Decoding_Machine(int num_rows, int num_columns) {
         this.num_rows = num_rows;
         this.num_columns = num_columns;
+        this.key_grid = new char[num_rows][num_columns];
     }//constructor
-    
+
+    //fills 2D Array key grid
     public void fill_key_grid(Iterator<Character> char_iterator) {
+        for(int i = 0; i < num_rows; i++){
+            for(int j = 0; j < num_columns; j++){
+                key_grid[i][j] = char_iterator.next();
+            }
+        }
 
     }//fill key grid
 
     public Iterator<Character> decode_message(Iterator<Key_Grid_Element> msg_iterator) {
 
+        //secret message
+        this.secret_message = new ArrayList<>();
+
+        while (msg_iterator.hasNext()) {
+            Key_Grid_Element temp_element = msg_iterator.next();
+            char temp_char = this.key_grid[temp_element.get_row()][temp_element.get_column()];
+            secret_message.add(temp_char);
+        }
+        return secret_message.iterator();
+
     }//decode message
 
+    //prints values in key grid
     public void print_key_grid() {
+        System.out.println("Key Grid Values: \n");
+        for(int i = 0; i < this.num_rows; i++){
+            for(int j = 0; j < this.num_columns; j++){
+                System.out.print(this.key_grid[i][j]);
+            }
+            System.out.println();
+        }
 
     }//print key grid
 }
